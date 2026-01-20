d556d8ba36a0f390309f6cd1f090156e14a349c6
wadhwarochak <rochak.wadhwa@gmail.com>
2026-01-20 07:24:17 +0530

    Focus - 20

---
+++ Pandas/01-Read-Tabular-Data.py
@@ -0,0 +1,3 @@
+import pandas as pd
+pd.read_table('data/chipotle.tsv', sep='\t')
+print(pd.read_table('data/chipotle.tsv', sep='\t'))
---
+++ PyTest/test_login.py
@@ -0,0 +1,8 @@
+def testLogin():
+    print("Login Successful")
+
+def testLogOff():
+    print("Logoff Successful")
+
+def testCalculation():
+    assert 2+2 ==4
